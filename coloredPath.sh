ca4274814a48a51e01fd437849e2e44882b31049
Bouteiller [a2n] Alan <a2n.dev@pm.me>
2026-09-11 17:59:25 +0200

    hue: fix some index error and interpolate

--- coloredPath.sh
+++ coloredPath.sh
@@ -9,44 +9,27 @@ else
   COLOR_SUPPORT=false
 fi
 
-hues=(
-  "255 100 000" # orange
-  "000 200 255" # blue
-  "200 000 255" # purple
-  "000 255 100" # green
-  "255 000 100" # pink
-  "255 255 000" # yellow
-)
-
-colors=(
-  $'\e[38;5;226m' $'\e[38;5;220m'
-  $'\e[38;5;214m' $'\e[38;5;208m'
-  $'\e[38;5;202m' $'\e[38;5;166m'
-  $'\e[38;5;130m'
-)
-
-getDegOfColors() {
-  r1=$1 g1=$2 b1=$3
-  r2=$(( 255 - r1 ))
-  g2=$(( 255 - g1 ))
-  b2=$(( 255 - b1 ))
-  steps=${7:-10}
-
-  colors=()
-
-  # lerp of the color
-  for i in $steps; do
-    r=$(( r1 + (r2 - r1) * i / (steps - 1) ))
-    g=$(( g1 + (g2 - g1) * i / (steps - 1) ))
-    b=$(( b1 + (b2 - b1) * i / (steps - 1) ))
-    colors+=($'\e[38;2;'"${r};${g};${b}"'m')
-  done
-}
-
 colorMyPath() {
-  reset=$'\e[0m'
+  hues=(
+    "255 100 000" # orange
+    "000 200 255" # blue
+    "200 000 255" # purple
+    "000 255 100" # green
+    "255 000 100" # pink
+    "255 255 000" # yellow
+  )
+
+  colors=(
+    $'\e[38;5;226m' $'\e[38;5;220m'
+    $'\e[38;5;214m' $'\e[38;5;208m'
+    $'\e[38;5;202m' $'\e[38;5;166m'
+    $'\e[38;5;130m'
+  )
+
   colored=""
   hue_idx=0
+  reset=$'\e[0m'
+
   if [ "$COLOR_SUPPORT" = true ]; then
       colors=()
   fi
@@ -54,8 +37,8 @@ colorMyPath() {
   IFS=":" read -ra paths <<< "$PATH"
 
   for path_idx in "${!paths[@]}"; do
-
     color_idx=0
+    hasEmpty=false
     IFS="/" read -ra parts <<< "${paths[$path_idx]}"
 
     if [ "$COLOR_SUPPORT" = true ]; then
@@ -86,29 +69,38 @@ colorMyPath() {
     for part_idx in "${!parts[@]}"; do
       # skip empty part
       if [ -z "${parts[$part_idx]}" ]; then
+        hasEmpty=true
         continue;
+      fi
+
+      l=${#parts[@]}
+      if [ "$hasEmpty" = true ]; then
+        l=$(( l - 1 ))
       fi
 
       if [ "$COLOR_SUPPORT" = true ]; then
         # interpolate color
-        r=$(( $r1 + ($r2 - $r1) * $color_idx / (${#parts[@]} - 1) ))
-        g=$(( $g1 + ($g2 - $g1) * $color_idx / (${#parts[@]} - 1) ))
-        b=$(( $b1 + ($b2 - $b1) * $color_idx / (${#parts[@]} - 1) ))
+        r=$(( $r1 + ($r2 - $r1) * $color_idx / ($l - 1) ))
+        g=$(( $g1 + ($g2 - $g1) * $color_idx / ($l - 1) ))
+        b=$(( $b1 + ($b2 - $b1) * $color_idx / ($l - 1) ))
         colors+=($'\e[38;2;'"${r};${g};${b}"'m')
       fi
+
+      colored="${colored}/${colors[$color_idx]}${parts[$part_idx]}$reset"
 
       if [ "$color_idx" -eq $(( ${#colors[@]} - 1 )) ]; then
         color_idx=0
       else
         color_idx=$(( color_idx + 1 ))
       fi
-
-      colored="${colored}/${colors[$color_idx]}${parts[$part_idx]}$reset"
     done
 
     if [ "$path_idx" -ne $(( ${#paths[@]} - 1 )) ]; then
       colored="${colored}:"
     fi
+
+    hasEmpty= false
+    colors=()
   done
 
   echo "$colored"
