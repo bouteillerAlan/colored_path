dc46943c12003567207ef8c7c333ccb123618157
Bouteiller [a2n] Alan <a2n.dev@pm.me>
2026-09-14 09:49:12 +0200

    hues: add better step and more hue

--- coloredPath.sh
+++ coloredPath.sh
@@ -11,12 +11,24 @@ fi
 
 colorMyPath() {
   hues=(
+    "255 000 000 255 120 120" # red → light red
+    "000 150 150 120 220 220" # teal → light teal
+    "255 080 050 255 160 140" # coral → light coral
+    "000 255 255 150 255 255" # cyan → light cyan
     "255 100 000 255 180 000" # orange → light orange
     "000 200 255 120 230 255" # blue → light blue
+    "255 180 000 255 220 120" # amber → light amber
+    "075 000 200 150 120 255" # indigo → light indigo
+    "255 255 000 255 255 150" # yellow → light yellow
+    "130 000 255 190 150 255" # violet → light violet
+    "150 255 000 210 255 120" # lime → light lime
     "200 000 255 230 120 255" # purple → light purple
     "000 255 100 120 255 170" # green → light green
+    "255 000 255 255 150 255" # magenta → light magenta
+    "000 255 150 150 255 200" # mint → light mint
     "255 000 100 255 120 180" # pink → light pink
-    "255 255 000 255 255 150" # yellow → light yellow
+    "000 200 180 150 255 230" # turquoise → light turquoise
+    "220 020 060 255 140 160" # crimson → light crimson
   )
   colors=(
     $'\e[38;5;226m' $'\e[38;5;220m'
@@ -44,12 +56,11 @@ colorMyPath() {
       selectedHue=${hues[$hue_idx]}
 
       r1=${selectedHue:0:3}
-      r2=${selectedHue:4:3}
+      g1=${selectedHue:4:3}
+      b1=${selectedHue:8:3}
 
-      g1=${selectedHue:8:3}
-      g2=${selectedHue:12:3}
-
-      b1=${selectedHue:16:3}
+      r2=${selectedHue:12:3}
+      g2=${selectedHue:16:3}
       b2=${selectedHue:20:3}
 
       if [[ "$r1" == "000" ]]; then
@@ -92,9 +103,9 @@ colorMyPath() {
 
       if [ "$COLOR_SUPPORT" = true ]; then
         # interpolate color
-        r=$(( $r1 + ($r2 - $r1) * $color_idx / ($l - 1) ))
-        g=$(( $g1 + ($g2 - $g1) * $color_idx / ($l - 1) ))
-        b=$(( $b1 + ($b2 - $b1) * $color_idx / ($l - 1) ))
+        r=$(( 10#$r1 + (10#$r2 - 10#$r1) * $color_idx / ($l - 1) ))
+        g=$(( 10#$g1 + (10#$g2 - 10#$g1) * $color_idx / ($l - 1) ))
+        b=$(( 10#$b1 + (10#$b2 - 10#$b1) * $color_idx / ($l - 1) ))
         colors+=($'\e[38;2;'"${r};${g};${b}"'m')
       fi
 
